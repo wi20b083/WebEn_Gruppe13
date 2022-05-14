c0a3c6c91677e96688dc43e8ee7f368f05418a1d
wi20b083 <wi20b083@technikum-wien.at>
2022-05-14 23:01:18 +0200

    Registration finished

--- backend/config/dbconnect.php
+++ backend/config/dbconnect.php
@@ -1,6 +1,6 @@
 <?php
 //setting up database access
-require_once ($GLOBALS["path"].'/backend/config/dbaccess.php');
+require_once ('dbaccess.php');
 $db_obj = new mysqli($host, $user, $password, $database);
 
 //if there is a connection error, echo a message and exit
