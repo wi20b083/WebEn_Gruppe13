ac8a4f28fb11db10661366cf91b38c98bb45f4ee
wi20b083 <wi20b083@technikum-wien.at>
2022-05-25 12:36:31 +0200

    Zwischenüberprüfung

---
+++ frontend/nav/footer.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+    <footer class="bg-primary text-white mt-auto">
+<!-- Grid container -->
+<div class="container p-3">
+            <!--Grid row-->
+            <div class="row">
+                <!--Grid column-->
+                <div class="col-lg-6 col-md-12 mb-4 mb-md-0 ">
+                    <h5 class="text-uppercase">Clothing-Gs</h5>
+
+                    <p>
+                    founded by Florian Huber, Akib Kahn and Nils Petsch (2022) <br>  Adress: Hochstädterplatz 6 1200 Wien  <br> phone number: +43 676 123123123 <br>e-mail: clothing-gs@webshop.com
+                    </p>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase"></h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href=".../index.html" class="text-white">Home</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Products</a>
+                        </li>
+                        <li>
+                            <a href="../sites/imprint.php" class="text-white">Imprint/About us</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Register/Login</a> <!-- if abfrage für Register/Login/Meinkonto-->
+                        </li>
+                    </ul>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase "></h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!" class="text-white">My Account</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Shopping cart</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 3</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Link 4</a>
+                        </li>
+                    </ul>
+                </div>
+                <!--Grid column-->
+            </div>
+            <!--Grid row-->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2022 Copyright:
+            <a class="text-white" href="#!">www.clothing-gs-webshop.com</a>
+        </div>
+        <!-- Copyright -->
+
+    </footer>
+    <!-- Footer -->
+
+
+
+</body>
+
+</html>
---
+++ frontend/sites/imprint.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imprint Clothing-Gs</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <title>Imprint Clothing-Gs</title>
+
+</head>
+<body>
+
+
+
+<div id= "ImprintDiv1" class ="container border border-dark rounded text-center mt-5 pt-5 mb-5 " >
+
+    <div id = "headerDivImprint"class= "container mb-5">
+    <h1 id = "header1Imprint"class= "fw-bold">IMPRINT</h1>
+    </div>
+
+    <hr>
+
+    <img src="..\res\img\logo-clothing-gsrichtig.png" alt="" class="pt-3">
+
+    <div>
+
+        <ul class = "list-unstyled">
+            <li id="imprintName" class = "fs-4 fw-bold pt-4">Webshop Clothing-Gs</li>
+            <li class = "fw-bold pt-5">ADRESS: </li>
+            <li id = "imprintAdress" class = "pt-2  ">Hochstädterplatz 6 <br> 1200 Wien, Österreich</li>
+            <li class = "fw-bold pt-5">CONTACT:</li>
+            <li id = "imprintContactWebshop"class = "pt-2">Webshop phone number: +43 676 123123123 </li>
+            <li id = "imprintContactEmail" class = "pt-2"> Webshop E-Mail: clothing-gs@webshop.com </li>
+            <li class="fw-bold pt-5"> UID-NUMBER:</li>
+            <li id ="imprintUidNumber" class= "pt-2">123123123</li>
+            <li class = "fw-bold pt-5">CHAMBER MEMBERSHIP:</li>
+            <li id="imprintChamberMembership" class="pt-2" >Regional court Vienna </li>
+            <li class = "fw-bold pt-5">Employees:</li>
+            <li class = "pt-2" >Florian Huber, Akib Kahn, Nils Petsch</li>
+            <img src="" alt="FotoFlo">
+            <img src="" alt="FotoAkib">
+            <img src="" alt="FotoNils">
+
+
+
+        </ul>
+        
+    </div>
+
+    <div id = "aboutUs">
+        <div class = "fs-5 fw-bold pt-5">ABOUT US</div>
+        <div id = "ImprintAboutUs" class ="pt-2">  Our <strong>MISSION</strong> is to let your own style run wild. <br> Besides creative designs and comfortable clothing, we want everyone to find their own style with us!  </div>
+        <div id = "ImprintAboutUsInfo" class ="pt-4 pb-5" > <strong>Clothing-Gs</strong> was founded in 2022 by the managing director Florian Huber, Akib Kahn und Nils Petsch. <br>  based in Vienna (Austria) </div>
+    </div>
+
+</div>
+
+
+    
+</body>
+
+<footer>
+    <?php include "../sites/footer.php" ?>
+</footer>
+</html>
